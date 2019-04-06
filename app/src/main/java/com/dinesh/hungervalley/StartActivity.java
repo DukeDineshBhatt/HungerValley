2de98f6f47dac1a3db9d4de0adf178b5cedf4ecb
Dukedinesh <Dukedineshbhatt@gmail.com>
2019-04-06 20:33:23 +0530

    splash edition

---
+++ app/src/main/java/com/dinesh/hungervalley/StartActivity.java
@@ -0,0 +1,60 @@
+package com.dinesh.hungervalley;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Point;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Display;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class StartActivity extends AppCompatActivity {
+
+    Button login, signup, skip;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_start);
+
+        login = (Button) findViewById(R.id.login);
+        signup = (Button) findViewById(R.id.signup);
+        skip = (Button) findViewById(R.id.skip);
+
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+        skip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(StartActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+
+
+            }
+        });
+    }
+}
